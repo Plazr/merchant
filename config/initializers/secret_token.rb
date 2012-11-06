64e9d5504a5169da397b935469eba023c64e84c1
Cedric Pimenta <cedricpim@gmail.com>
2012-11-06 16:12:21 +0000

    initial commit for activemerchant gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merchant::Application.config.secret_token = '98d0d4f41003d8d924e5518cb7420f380893ac6d024b9827947c8e065a0d287c1d61e15ec4c564c84fd8c341ba0abdbebb412917714e0fa1331c4e4ad1af5e57'
